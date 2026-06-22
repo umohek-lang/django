f4de0454ee8a099e6eea0dd067c4770fd2276469
umohekpo <umohek@gmail.com>
2026-06-22 11:25:47 +0100

    Update Django settings for Render deployment

--- blog/settings.py
+++ blog/settings.py
@@ -10,17 +10,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@b4#6f)kb-4mpm)5lqt4a&=o7-s8agg)nz7n@e5i$*72xdj9m3'
+# SECRET_KEY = 'django-insecure-@b4#6f)kb-4mpm-1lqt4a&=o7-s8agg9nz7n@e5i$*72xdj9m3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@b4#6f)kb-4mpm-1lqt4a&=o7-s8agg9nz7n@e5i$*72xdj9m3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == "True"
 
-ALLOWED_HOSTS = ['mysite.com']
+ALLOWED_HOSTS = [
+    'mysite.com',
+    '127.0.0.1',
+    'localhost',
+    'myblogpost-m946.onrender.com',
+    '.onrender.com'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-# Application definition
 
+
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,9 +45,8 @@ INSTALLED_APPS = [
 ]
 
 LOGIN_REDIRECT_URL = '/'   # Take user to home page
-LOGOUT_REDIRECT_URL = '/' # tell django to log out
-LOGIN_URL = '/users/login' # Direct user to login page who don't have access to any page
-
+LOGOUT_REDIRECT_URL = '/'  # tell django to log out
+LOGIN_URL = '/users/login' # Direct user to login page who don't have access
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -76,20 +84,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASE
+if not DEBUG:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL")
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -107,29 +118,23 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+# STATIC FILES
 STATIC_URL = '/static/'
-
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStsticFilesStorage"
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# Heroku compatibility (kept as you requested)
 django_heroku.settings(locals())
